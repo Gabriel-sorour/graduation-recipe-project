5306860b7eee8ec081e1d8abd377647ca399023a
Gabriel-sorour <gabrielsorour51@gmail.com>
2025-11-29 19:39:56 +0200

    Add Link to the Landing page and update header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,9 @@ function Header() {
   return (
     <header className='app-header container'>
       <ul className='nav-links'>
+        <Link to="/"><li>
+          <span>COOK</span>WHAT<span>U</span>HAVE
+        </li></Link>
         <Link to="/Home"><li>Home</li></Link>
         <Link to="/about"><li>About</li></Link>
       </ul>

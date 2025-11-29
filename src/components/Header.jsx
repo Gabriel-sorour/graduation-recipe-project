882d65b30060d836d94fccca9be665d243607692
Gabriel-sorour <gabrielsorour51@gmail.com>
2025-11-29 19:27:21 +0200

    refactor: use Link instead of <a>

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
+import { Link } from 'react-router-dom'
 import '../styles/Header.css'
 
 function Header() {
   return (
     <header className='app-header container'>
       <ul className='nav-links'>
-        <li><a href="#/Home">Home</a></li>
-        <li><a href="#/about">About</a></li>
+        <Link to="/Home"><li>Home</li></Link>
+        <Link to="/about"><li>About</li></Link>
       </ul>
     </header>
   )

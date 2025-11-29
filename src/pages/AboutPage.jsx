a42b1200eb22e5e95e78fb069c2d54aa6d80898f
Gabriel-sorour <gabrielsorour51@gmail.com>
2025-11-29 22:27:10 +0200

    Add favicons to about and home pages

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -4,7 +4,8 @@ function AboutPage() {
   return (
     <>
       <title>About</title>
-      
+      <link rel="icon" type="image/svg+xml" href="about-favicon.png" />
+
       <div className="container">
         <h2>About Page</h2>
         <p>This is the about page of the application.</p>

d42390226b8d32edb9a54acaff9cba96c96138c4
Gabriel-sorour <gabrielsorour51@gmail.com>
2025-11-30 17:27:27 +0200

    feat: create NotFoundPage

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Footer from './components/Footer.jsx'
 import HomePage from './pages/HomePage.jsx'
 import AboutPage from './pages/AboutPage.jsx'
 import LandingPage from './pages/LandingPage.jsx'
+import NotFoundPage from './pages/NotFoundPage.jsx'
 import './App.css'
 
 
@@ -16,6 +17,7 @@ function App() {
         <Route index element={<LandingPage />} />
         <Route path='Home' element={<HomePage />} />
         <Route path='about' element={<AboutPage />} />
+        <Route path='*' element={<NotFoundPage />} />
       </Routes>
       <Footer />
     </div>
